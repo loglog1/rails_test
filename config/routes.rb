8a4f1d194b01f2c1a927a4555fb7a541fe20e565
loglog1 <bassai41@gmail.com>
2023-10-01 22:56:35 +0900

    エラーメッセージでちょっと遊んでみた

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
 end
